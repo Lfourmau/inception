89abd14c367ce2536320ad9235f671f82c981858
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-01-10 18:46:52 +0100

    nginx alays available, trying to add some config in nginx, mariadb and wordpress but without success

---
+++ srcs/requirements/mariadb/tools/wp_database.sql
@@ -0,0 +1,6 @@
+# mysql -uroot -p
+CREATE DATABASE IF NOT EXISTS ft_server DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+GRANT ALL ON *.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+FLUSH PRIVILEGES;
+\q
