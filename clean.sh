e2cb6a26f2f4245a4a1f4045f83bd2f89f3b1f58
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-02-09 09:57:48 +0100

    script to clean. Wordpress works, need to connect it to bdd now.

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker system prune -a
+echo -----------VOLUMES------------
+docker volume ls
+echo -----------NETWORKS------------
+docker network ls
+echo -----------CONTAINERS------------
+docker ps -a
+echo -----------IMAGES------------
+docker image ls
+rm -rf wp_data
---
+++ srcs/requirements/wordpress/tools/config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mv /var/www/wp/wordpress /var/www/html/wordpress
+service php7.3-fpm start
+tail -f
