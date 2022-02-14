c623cf7367c71a9e941c8f63bf08a922d04b65ec
Lfourmau <loicfourmaux@gmail.com>
2022-02-14 14:02:47 +0100

    fix #4 and try to clean volume by deleting mv and cp steps. Don't know why but target folders are no longer erased by the volume

--- srcs/requirements/mariadb/tools/wp_database.sql
+++ srcs/requirements/mariadb/tools/wp_database.sql
@@ -1,6 +1,6 @@
 # mysql -uroot -p
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpress';
 GRANT ALL ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
 FLUSH PRIVILEGES;
 \q
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mv /var/www/wp/wordpress /var/www/html/wordpress
+
+#mv /var/www/wp/wordpress /var/www/html/wordpress
 service php7.3-fpm start
 tail -f
