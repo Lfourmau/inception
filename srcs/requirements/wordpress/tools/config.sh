5713885a41f6640b2adfe75637838bdb1863b5c2
Lfourmau <loicfourmaux@gmail.com>
2022-02-15 10:27:17 +0100

    wp-cli script seems to work. 2users are created and install pag is gone. I don't have the navbar, weird. Mysql start only afeter e big clean + docker restarting

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-#mv /var/www/wp/wordpress /var/www/html/wordpress
 service php7.3-fpm start
+cd /var/www/html/wordpress/
+rm wp-config-sample.php
+wp core install --url=localhost --title=Titletest --admin_user=“boss” --admin_password=1234 --admin_email=boss@boss.fr --allow-root
+wp language core activate fr --allow-root
+wp user create lfourmau lolo@lolo.fr --role=author --user_pass=1234 --allow-root
 tail -f
