7f24265751ca623e4d222747dd83078509c231bd
Lfourmau <loicfourmaux@gmail.com>
2022-02-21 12:10:50 +0100

    Delete tail -f in wordpress ctnr. Docker sucks on my post try to change

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -8,4 +8,5 @@ sed -i "s/dbuser/$WORDPRESS_DB_USER/g" /var/www/html/wordpress/wp-config.php
 sed -i "s/dbpassword/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
 wp core install --url=localhost --title=MySuperMegaWebsite --admin_user=“boss” --admin_password=1234 --admin_email=boss@boss.fr --allow-root
 wp user create lfourmau lolo@lolo.fr --role=author --user_pass=1234 --allow-root
-tail -f
+service php7.3-fpm stop
+php-fpm7.3 -F -R
