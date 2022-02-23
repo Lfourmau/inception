7794afcbcc5960273a4cb1d62764346dba56c2a6
Lfourmau <loicfourmaux@gmail.com>
2022-02-23 09:52:36 +0100

    change localhost to lfourmau.42.fr and fix bug in congif_flag creation

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-FILE=./config_flag
-if [ -f "$FILE" ]; then
+FILE=config_flag
+if [ -f "/$FILE" ]; then
     echo "Existing config installed"
 else 
 	service php7.3-fpm start
@@ -9,7 +9,7 @@ else
 	sed -i "s/dbname/$WORDPRESS_DB_NAME/g" /var/www/html/wordpress/wp-config.php
 	sed -i "s/dbuser/$WORDPRESS_DB_USER/g" /var/www/html/wordpress/wp-config.php
 	sed -i "s/dbpassword/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
-	wp core install --url=localhost --title=MySuperMegaWebsite --admin_user=“boss” --admin_password=1234 --admin_email=boss@boss.fr --allow-root
+	wp core install --url=lfourmau.42.fr --title=MySuperMegaWebsite --admin_user=“boss” --admin_password=1234 --admin_email=boss@boss.fr --allow-root
 	wp user create lfourmau lolo@lolo.fr --role=author --user_pass=1234 --allow-root
 	service php7.3-fpm stop
 	cd / && touch config_flag
