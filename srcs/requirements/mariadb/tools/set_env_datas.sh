dba3d1c3e3108ba1e07d2e6b0634246ad08c3858
Lfourmau <loicfourmaux@gmail.com>
2022-02-23 08:46:27 +0100

    set var envs by add ARG in dockerfile. Stop to exec wpcli script is container is relaunched

--- srcs/requirements/mariadb/tools/set_env_datas.sh
+++ srcs/requirements/mariadb/tools/set_env_datas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s/mysql_root_pw/1234/g" /scripts/wp_database.sql
-sed -i "s/mysql_database/wordpress/g" /scripts/wp_database.sql
-sed -i "s/mysql_user/wordpress/g" /scripts/wp_database.sql
-sed -i "s/mysql_pw/wordpress/g" /scripts/wp_database.sql
+sed -i "s/mysql_root_pw/$ROOT_PASSWORD/g" /scripts/wp_database.sql
+sed -i "s/mysql_database/$DB_NAME/g" /scripts/wp_database.sql
+sed -i "s/mysql_user/$DB_USER/g" /scripts/wp_database.sql
+sed -i "s/mysql_pw/$DB_PASSWORD/g" /scripts/wp_database.sql
--- srcs/requirements/mariadb/tools/wp_database.sql
+++ srcs/requirements/mariadb/tools/wp_database.sql
@@ -1,11 +1,11 @@
 # mysql -uroot
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'wordpress';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'mysql_root_pw';
 --mysqladmin -u root password "1234"
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpress';
-GRANT ALL ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+CREATE DATABASE IF NOT EXISTS mysql_database;
+CREATE USER IF NOT EXISTS 'mysql_user'@'%' IDENTIFIED BY 'mysql_pw';
+GRANT ALL ON *.* TO 'mysql_user'@'%' IDENTIFIED BY 'mysql_pw';
 FLUSH PRIVILEGES;
 \q
