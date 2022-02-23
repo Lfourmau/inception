f568087e360bcb82fd7bb44e5e273aebae4c31a5
Lfourmau <loicfourmaux@gmail.com>
2022-02-23 10:17:07 +0100

    add php start and stop in config

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+service php7.3-fpm start
 FILE=/var/www/html/wordpress/wp-config.php
 if [ -f "/$FILE" ]; then
     echo "Existing config installed"
 else 
-	service php7.3-fpm start
 	cd /var/www/html/wordpress/
 	rm wp-config-sample.php
 	sed -i "s/dbname/$WORDPRESS_DB_NAME/g" /var/www/html/wordpress/wp-config.php
@@ -11,6 +11,6 @@ else
 	sed -i "s/dbpassword/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
 	wp core install --url=lfourmau.42.fr --title=MySuperMegaWebsite --admin_user=“boss” --admin_password=1234 --admin_email=boss@boss.fr --allow-root
 	wp user create lfourmau lolo@lolo.fr --role=author --user_pass=1234 --allow-root
-	service php7.3-fpm stop
 fi
+service php7.3-fpm stop
 php-fpm7.3 -F -R
